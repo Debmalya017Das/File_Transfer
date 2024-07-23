ba4087d5f5c48b203659c426875828509ca53b9c
Debmalya Das <106435168+Debmalya017Das@users.noreply.github.com>
2024-07-23 12:36:03 +0530

    Update server.cjs

--- server/server.cjs
+++ server/server.cjs
@@ -13,7 +13,8 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+     origin: "https://file-transfer-1.onrender.com",
     methods: ["GET", "POST"]
   }
 });
